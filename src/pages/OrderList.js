b58c49272216666b6dc5b00e92664aa483f6cdf3
krishn157 <krishn.kumar2018@vitstudent.ac.in>
2022-09-26 17:27:42 +0530

    material-table-added

--- src/pages/OrderList.js
+++ src/pages/OrderList.js
@@ -1,7 +1,20 @@
+import { CCard, CCardBody, CCardHeader } from "@coreui/react";
 import React from "react";
+import CustomTable from "../components/CustomTable";
 
 const OrderList = () => {
-  return <div>OrderList</div>;
+  return (
+    <>
+      <CCard className="mb-4">
+        <CCardHeader>
+          <strong>Order Requests</strong>
+        </CCardHeader>
+        <CCardBody>
+          <CustomTable />
+        </CCardBody>
+      </CCard>
+    </>
+  );
 };
 
 export default OrderList;
