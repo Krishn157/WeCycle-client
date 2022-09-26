923aef1e12931e99ed692ee08290f056ce81fb7b
krishn157 <krishn.kumar2018@vitstudent.ac.in>
2022-09-26 17:41:06 +0530

    table-dummy-data

--- src/components/CustomTable.js
+++ src/components/CustomTable.js
@@ -6,22 +6,43 @@ const CustomTable = () => {
     <MaterialTable
       title="Positioning Actions Column Preview"
       columns={[
-        { title: "Name", field: "name" },
-        { title: "Surname", field: "surname" },
-        { title: "Birth Year", field: "birthYear", type: "numeric" },
+        { title: "Producer Name", field: "name" },
+        { title: "Waste Type", field: "type" },
+        { title: "Primary Substance", field: "substance" },
+        { title: "Quantity", field: "quantity", type: "numeric" },
         {
-          title: "Birth Place",
-          field: "birthCity",
-          lookup: { 34: "İstanbul", 63: "Şanlıurfa" },
+          title: "Requested On",
+          field: "reqDate",
         },
       ]}
       data={[
-        { name: "Mehmet", surname: "Baran", birthYear: 1987, birthCity: 63 },
         {
-          name: "Zerya Betül",
-          surname: "Baran",
-          birthYear: 2017,
-          birthCity: 34,
+          name: "Company 1",
+          type: "Type-1",
+          substance: "Substance1",
+          quantity: "100kg",
+          reqDate: "12/02/2021",
+        },
+        {
+          name: "Company 2",
+          type: "Type-1",
+          substance: "Substance2",
+          quantity: "100kg",
+          reqDate: "02/07/2022",
+        },
+        {
+          name: "Company 3",
+          type: "Type-2",
+          substance: "Substance4",
+          quantity: "100kg",
+          reqDate: "21/08/2022",
+        },
+        {
+          name: "Company 4",
+          type: "Type-3",
+          substance: "Substance3",
+          quantity: "90kg",
+          reqDate: "12/09/2022",
         },
       ]}
       actions={[
